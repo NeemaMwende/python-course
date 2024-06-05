48122b74843e9a9e7041383f2bbce59fbca92d5f
NeemaMwende <gracepete12@gmail.com>
2024-06-05 15:36:30 +0300

    updates

---
+++ intro.py
@@ -0,0 +1,23 @@
+# x = 10
+
+# if x > 5:
+#     print("x is greater than 5")
+# else:
+#     print("x is less than or equal to 5")
+
+# y = 3;
+# c = 7;
+# if y > 3 and c > 5:
+#     print("y > 3 and c > 5")
+# elif y <= 3 or c <= 5:
+#         print("y <= 3 or c <= 5")
+# else:
+#     print("y <= 3 or c <= 5") 
+
+# for i in range(5):
+#     print(i)
+
+# import sys
+# print(sys.version)
+
+print("Cheers mate!")
