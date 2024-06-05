6884502300a5771d9946d37cd0bc18db3cdb07ef
NeemaMwende <gracepete12@gmail.com>
2024-06-05 22:53:56 +0300

    updates

---
+++ data.py
@@ -0,0 +1,29 @@
+# x = frozenset({"apple", "banana", "cherry"})
+# print(x)
+
+# y = memoryview(bytes(5))
+# print(y)
+
+# x = ("apple", "banana", "cherry")
+# print(x)
+
+# x = dict(name="John", age=36)
+# print(x)
+
+# x = 1
+# y = 35656222554887711
+# z = -3255522
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+
+# print(type(x))
+# print(x)
+# print(type(y))
+# print(type(z))
+
--- intro.py
+++ intro.py
@@ -68,3 +68,30 @@
 # def myfunc():
 #     print("Charles is" + y)
 # myfunc()
+
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+# def myfunc():
+#     global x
+#     x = "fantastic"
+# myfunc()
+# print("Python is " + x)
+
+# x = "awesome"
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
