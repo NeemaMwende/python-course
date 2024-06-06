053a7c0f0cd7c7ae9132e61c6c6d04f5589eed1f
NeemaMwende <gracepete12@gmail.com>
2024-06-06 15:41:11 +0300

    negative indexing

--- strings.py
+++ strings.py
@@ -34,8 +34,11 @@
 
 #Slicing
 # Get the characters from position 2 to position 5 (not included):
-b = "Hello, World!"
-print(b[2:12])
+# b = "Hello, World!"
+# print(b[2:12])
+
+# b = "Hello, World!"
+# print(b[2:])
 
 b = "Hello, World!"
-print(b[2:])
+print(b[-5:-2])
