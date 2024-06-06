662a3f64605a6a8f4ba2dd72d4688a5584553b95
NeemaMwende <gracepete12@gmail.com>
2024-06-06 22:11:03 +0300

    boolen

--- boolen.py
+++ boolen.py
@@ -1,7 +1,16 @@
-a = 200
-b = 100
+# a = 200
+# b = 100
 
-if a > b: 
-    print("a is greater than b")
-else:
-    print("a is not greater than b")
+# if a > b: 
+#     print("a is greater than b")
+# else:
+#     print("a is not greater than b")
+
+# print(bool("Hello"))
+# print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
