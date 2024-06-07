b412953b3b5c6378041e16fc71bdcc1164ed05bd
NeemaMwende <gracepete12@gmail.com>
2024-06-07 19:06:12 +0300

    updates

--- operators.py
+++ operators.py
@@ -1,4 +1,58 @@
-print (10 ** 3)
-print((6 + 3) - (6 + 3))
-print(100 + 5 * 3)
-print(5 + 4 - 7 + 3)
+# print (10 ** 3)
+# print((6 + 3) - (6 + 3))
+# print(100 + 5 * 3)
+# print(5 + 4 - 7 + 3)
+
+#LISTS
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
+# list4 = ["abc", 34, True, 40, "male"]
+# print(list1,list2,list3,list4)
+
+# mylist = ["apple", "banana", "cherry", "apple", "cherry", "apple", "cherry", "hub"]
+# print(type(mylist))
+
+# thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-1])
+# print(mylist[1:5])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:4])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+#change item value
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
